452f38f25e1400fe8654eb5d038ad2c15503b1e0
sbillu <somusekharbillu@gmail.com>
2025-04-24 09:51:35 +0530

    modified the files

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 this is a w terraform file
+=======
+module "VPC" {
+    source          = "https://github.com/Somu7/module.vpc.git?ref=development"
+    name            =var.name
+    environment     = var.environment
+}
+>>>>>>> e7ab12f (added the new file)
