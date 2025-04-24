2b8da9b6bc6d0390cce92a691d34c7703b8359d3
sbillu <somusekharbillu@gmail.com>
2025-04-24 15:07:57 +0530

    add

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
-  source      = "git::git@github.com:Somu7/module.vpc.git"
+  source      = "git::git@github.com:Somu7/module.vpc.git?ref=development"
   region      = var.region
   vpc_cidr    = var.vpc_cidr
-  subnet_cidr = var.subnet_cidr
-  tags        = var.tags
+  subnet_cidr = var.subnet_cidr  # Ensure it's passed only if the module expects it
+  tags        = var.tags  
 }
