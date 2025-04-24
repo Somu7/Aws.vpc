5287c5df6f5c4793e266f511dd4c17c5ab059fc5
Somu7 <87271079+Somu7@users.noreply.github.com>
2025-04-24 09:31:08 +0530

    Adding config file

--- variable.tf
+++ variable.tf
@@ -1,14 +1,1 @@
-#General variable
-
-variable "name"{
-    description = "name of the cloud"
-}
-
-variable "region" {
-    description = "deloyed the resources in region"
-    default = "us-east-1"
-}
-
-variable "environment" {
-    description = "environment tag "
-}
+it is second file 
